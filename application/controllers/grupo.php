ab609d313e7d9d12e999218ad45771ab9ff3c875
lmartinez95 <luigi6884@gmail.com>
2018-05-03 18:07:04 -0600

    conf

---
+++ application/controllers/grupo.php
@@ -0,0 +1,68 @@
+<?php
+    class Grupo extends CI_Controller
+    {
+        public function __construct()
+        {
+            parent::__construct();
+            $this->load->model('grupo_model');
+            $this->load->helper('url_helper');
+        }
+        public function index($mensaje = NULL, $nivel = NULL){
+            $data['results'] = $this->grupo_model->mostrar();
+            $data['combo'] = $this->grupo_model->cargaCombo();
+            $data['title'] = 'Empleados';
+            if (isset($mensaje) && isset($nivel)) {
+                $data['mensaje'] = $mensaje;
+                $data['nivel'] = $nivel;
+            }
+            $this->load->view('shared/header', $data);
+            $this->load->view('empleado/index', $data);
+        }
+
+        public function agregar()
+        {
+            $data = array(
+                'empNombre' => $_REQUEST['txtNombre'],
+                'empApellidoP' => $_REQUEST['txtApellidoP'],
+                'empApellidoM' => $_REQUEST['txtApellidoM'],
+                'empSexo' => $_REQUEST['cboSexo'],
+                'empDUI' => $_REQUEST['txtDUI'],
+                'empNIT' => $_REQUEST['txtNIT'],
+                'empISSS' => $_REQUEST['txtISSS'],
+                'empNUP' => $_REQUEST['txtNUP'],
+                'empDireccion' => $_REQUEST['txtDireccion'],
+                'empEmail' => $_REQUEST['txtEmail'],
+                'tempId' => $_REQUEST['cboTempId'] );
+            $b = $this->grupo_model->agregar($data);
+            if (substr($b,0,3) === TRUE) {
+                $mensaje = "Registro agregado exitosamente. El codigo es ".substr($b,4);
+                $nivel = 'success';
+            } else {
+                $mensaje = $b;
+                $nivel = 'warning';
+            }
+            $this->index($mensaje,$nivel);
+        }
+
+        public function eliminar($value = null)
+        {
+            if (isset($value)) {
+                $b = $this->grupo_model->eliminar($value);
+                if ($b === TRUE) {
+                    $mensaje = "Registro eliminado exitosamente.";
+                    $nivel = 'success';
+                } else {
+                    $mensaje = $b;
+                    $nivel = 'warning';
+                }
+                
+            } else {
+                $mensaje = "Operación no válida";
+                    $nivel = 'danger';
+            }
+            $this->index($mensaje,$nivel);
+            
+        }
+    }
+    
+?>
---
+++ application/models/grupo_model.php
@@ -0,0 +1,68 @@
+<?php
+    class Grupo_model extends CI_Model
+    {
+        public function __construct(){
+            $this->load->database();
+        }
+
+        public function mostrar(){
+            $this->db->select("e.empId,e.empCodigo,CONCAT(e.empNombre,' ',e.empApellidoP,' ',e.empApellidoM) as Nombre,
+            CASE e.empSexo WHEN 'M' THEN 'Masculino' ELSE 'Femenino' END AS Sexo,e.empDUI,te.tempNombre");
+            $this->db->from('Empleado e');
+            $this->db->join('TipoEmpleado te', 'e.tempId = te.tempId');
+            $query = $this->db->get();
+            $this->db->close();
+            return $query->result_array();
+        }
+
+        public function agregar($data)
+        {
+            try{
+                $query = $this->db->query('CALL spAddEmpleado(?,?,?,?,?,?,?,?,?,?,?)', $data);
+                return 'true'.$query;
+            } catch(Exception $e){
+                return "ERROR. No se pudo ingresar el registro";
+            } finally{
+                $this->db->close();
+            }
+        }
+
+        public function cargaCombo()
+        {
+            try{
+                $this->db->select('tempId,tempNombre');
+                $this->db->from('TipoEmpleado');
+                $query = $this->db->get();
+                return $query->result_array();
+            } catch(Exception $e){
+                return false;
+            }
+        }
+
+        public function eliminar(int $value)
+        {
+            try{
+                $this->db->select('COUNT(empId) AS cant');
+                $this->db->from('Empleado');
+                $this->db->where('empId', $value);
+                $query = $this->db->get();
+                $result = $query->result_array();
+                foreach ($result as $r) {
+                    if ($r['cant'] == 1) {
+                        $this->db->where('empId', $value);
+                        $this->db->delete('Empleado');
+                        return true;
+                    } else {
+                        return "El código no existe";
+                    }
+                }
+            } catch(Exception $e){
+                return "ERROR. No se pudo eliminar";
+            } finally{
+                $this->db->close();
+            }
+        }
+
+        
+    }
+?>
